8a4a93ec3b4bcfce982c997bb28bc3222b060c40
Bailey Chave <bailey.chave@gmail.com>
2025-06-25 16:47:39 -0400

    added connection handling

--- main.go
+++ main.go
@@ -4,12 +4,44 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	fmt.Fprintf(conn, "220 localhost Simple SMTP Server\r\n")
-	// For production, you'd need full SMTP command handling (HELO, MAIL FROM, RCPT TO, DATA, QUIT, etc.)
+
+	conn.Write([]byte("220 localhost Simple SMTP Server\r\n"))
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Println("Connection closed:", err)
+			return
+		}
+
+		input := strings.TrimSpace(string(buf[:n]))
+		fmt.Println("Client:", input)
+
+		switch {
+		case strings.HasPrefix(input, "HELO"):
+			conn.Write([]byte("250 Hello\r\n"))
+		case strings.HasPrefix(input, "MAIL FROM:"):
+			conn.Write([]byte("250 OK\r\n"))
+		case strings.HasPrefix(input, "RCPT TO:"):
+			conn.Write([]byte("250 OK\r\n"))
+		case strings.HasPrefix(input, "DATA"):
+			conn.Write([]byte("354 End data with <CR><LF>.<CR><LF>\r\n"))
+		case input == ".":
+			conn.Write([]byte("250 Message received\r\n"))
+		case strings.HasPrefix(input, "QUIT"):
+			conn.Write([]byte("221 Bye\r\n"))
+			return
+		default:
+			conn.Write([]byte("500 Unrecognized command\r\n"))
+		}
+	}
 }
 
 func main() {
